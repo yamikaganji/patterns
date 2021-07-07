b72353d37c5a262d93449c0c3e10ed9b50a35be0
yamikaganji <86185074+yamikaganji@users.noreply.github.com>
2021-07-07 20:34:20 +0530

    Print * pattern
    given input n,print the pattern accordingly;
    ex  n=4;
    *
    * *
    * * *
    * * * *

---
+++ Pattern.java
@@ -0,0 +1,22 @@
+package basicjavaprograms;
+
+import java.util.Scanner;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter n value");
+		int  n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+
+}
